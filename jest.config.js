7010acbef2c62197446486fb7ad8585673794197
Tjelvar Olsen <tenestoro@googlemail.com>
2019-10-28 13:17:43 +0100

    jest fix

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     preset: 'react-native',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)x?$",
+    "globals": {
+      "window": {}
+    },
+    "transformIgnorePatterns": [
+      "node_modules/(?!react-native|react-router-native)/"
+    ],
 };
